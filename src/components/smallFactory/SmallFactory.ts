1e8a25d2116b749611c49f41753e7c20ad1256e3
Nastya <himeriya97@mgmail.com>
2023-02-08 09:19:45 +0300

    feat: add small factory

---
+++ src/components/smallFactory/SmallFactory.ts
@@ -0,0 +1,22 @@
+import Factory from '../factory/Factory';
+import { CONSTANTS } from './constants';
+import './smallFactory.scss';
+
+class SmallFactory extends Factory {
+  protected title: string;
+  protected description: string;
+
+  constructor() {
+    super(CONSTANTS.cookieProduction);
+    this.title = CONSTANTS.title;
+    this.description = CONSTANTS.description;
+  }
+
+  draw(): HTMLElement {
+    const factory = super.draw(this.title, this.description);
+
+    return factory;
+  }
+}
+
+export default SmallFactory;
---
+++ src/components/smallFactory/constants.ts
@@ -0,0 +1,6 @@
+export const CONSTANTS = {
+  cookieProduction: 10,
+  title: 'Small Factory',
+  description:
+    'Small Factory provides 10 cookies per second without upgrades. On the max level it can provide 50 cookies per second.',
+};
